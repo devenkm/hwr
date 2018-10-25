3773fad42507c553f99bb4843848f0bc9abc787a
devenkm <devendrakmishra@gmail.com>
2018-10-25 05:50:52 -0400

    Dynamic variables.

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Chance from 'chance';
 
 class Detail extends React.Component{
     render(){
-        return <p>{this.props.message} </p>
+        //return <p>{this.props.message} </p>
+        return <p>Hello, {chance.first()} </p>
+
     }
 }
 export default Detail;
